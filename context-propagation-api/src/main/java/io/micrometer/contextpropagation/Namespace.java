48fe871e5f9f0bd596248673a754725e36012ce4
Marcin Grzejszczak <mgrzejszczak@vmware.com>
2022-03-29 18:54:28 +0200

    First pass on filtering of accessors (#5)
    * First pass on filtering of accessors
    
    * Changes following the review

---
+++ context-propagation-api/src/main/java/io/micrometer/contextpropagation/Namespace.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2002-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micrometer.contextpropagation;
+
+/**
+ * A {@code Namespace} is used to provide a <em>scope</em> for data saved by
+ * extensions within a {@link Store}. The key should be dot-separated for a given
+ * family of keys. E.g.
+ *
+ * <ul>
+ *  <li>for Micrometer Observation you should use {@code micrometer.observation}</li>
+ *  <li>for Spring Data you should use {@code spring.data}</li>
+ *  <li>for Hibernate Transaction you should use {@code hibernate.transaction}</li>
+ * </ul>
+ *
+ * Then you can filter out whole family of keys e.g. don't propagate any spring related context.
+ *
+ * Inspired by JUnit Extension Namespace.
+ *
+ * @since 1.0.0
+ */
+public interface Namespace {
+
+    /**
+     * Key under which this namespace is registered.
+     *
+     * @return namespace key
+     */
+    default String getKey() {
+        return getClass().getName();
+    }
+
+    /**
+     * Creates a new instance of a {@link Namespace}.
+     *
+     * @param key key under which this namespace is to be registered
+     * @return new namespace
+     */
+    static Namespace of(String key) {
+        return new SimpleNamespace(key);
+    }
+}
---
+++ context-propagation-api/src/test/java/io/micrometer/contextpropagation/ObservationThreadLocalAccessor.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2002-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micrometer.contextpropagation;
+
+public class ObservationThreadLocalAccessor implements ThreadLocalAccessor {
+
+    public static final String KEY = "micrometer.observation";
+
+    public static final Namespace OBSERVATION = Namespace.of(KEY);
+
+    private final NamespaceAccessor<ObservationStore> namespaceAccessor = new NamespaceAccessor<>(OBSERVATION);
+
+    @Override
+    public void captureValues(ContextContainer container) {
+        String value = ObservationThreadLocalHolder.holder.get();
+        if (value != null) {
+            this.namespaceAccessor.putStore(container, new ObservationStore(value));
+        }
+    }
+
+    @Override
+    public void restoreValues(ContextContainer container) {
+        if (this.namespaceAccessor.isPresent(container)) {
+            ObservationStore store = this.namespaceAccessor.getStore(container);
+            String observation = store.getObservation();
+            ObservationThreadLocalHolder.holder.set(observation);
+        }
+    }
+
+    @Override
+    public void resetValues(ContextContainer container) {
+        if (this.namespaceAccessor.isPresent(container)) {
+            this.namespaceAccessor.getRequiredStore(container).close();
+        }
+    }
+
+    @Override
+    public Namespace getNamespace() {
+        return OBSERVATION;
+    }
+
+}
+
+class ObservationStore implements Store, AutoCloseable {
+    final String observation;
+
+    ObservationStore(String observation) {
+        this.observation = observation;
+    }
+
+    String getObservation() {
+        return this.observation;
+    }
+
+    @Override
+    public void close() {
+        ObservationThreadLocalHolder.holder.remove();
+    }
+}
+
