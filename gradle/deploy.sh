ac83bbc1e99c9ebb705136ed301281606f29f73f
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2024-01-26 13:04:20 -0800

    Test deploy to the commercial repo

--- gradle/deploy.sh
+++ gradle/deploy.sh
@@ -9,19 +9,20 @@ if [ $CIRCLE_PR_NUMBER ]; then
   echo -e "Not attempting to publish"
 elif [ -z $CIRCLE_TAG ]; then
   echo -e "Publishing Snapshot => Branch ['$CIRCLE_BRANCH']"
-  ./gradlew -Prelease.stage=SNAPSHOT snapshot publishNebulaPublicationToSnapshotRepository $SWITCHES
+  ./gradlew -Prelease.stage=SNAPSHOT snapshot publishNebulaPublicationToCommercialSnapshotRepository $SWITCHES
 elif [ $CIRCLE_TAG ]; then
   echo -e "Publishing Release => Branch ['$CIRCLE_BRANCH'] Tag ['$CIRCLE_TAG']"
   case "$CIRCLE_TAG" in
   *-M*)
-    ./gradlew -Prelease.disableGitChecks=true -Prelease.useLastTag=true -Prelease.stage=milestone candidate publishNebulaPublicationToMilestoneRepository $SWITCHES
+    echo -e "WARN: Milestone releases are disabled"
+    echo -e "Not attempting to publish"
     ;;
   *-RC*)
-     # -Prelease.stage=milestone instead of rc (should be rc), probably related to this bug: https://github.com/nebula-plugins/nebula-release-plugin/issues/213
-    ./gradlew -Prelease.disableGitChecks=true -Prelease.useLastTag=true -Prelease.stage=milestone candidate publishNebulaPublicationToMilestoneRepository $SWITCHES
+    echo -e "WARN: Milestone releases are disabled"
+    echo -e "Not attempting to publish"
     ;;
   *)
-    ./gradlew -Prelease.disableGitChecks=true -Prelease.useLastTag=true -Prelease.stage=final final publishNebulaPublicationToMavenCentralRepository closeAndReleaseMavenCentralStagingRepository $SWITCHES
+    ./gradlew -Prelease.disableGitChecks=true -Prelease.useLastTag=true -Prelease.stage=final final publishNebulaPublicationToCommercialReleaseRepository $SWITCHES
     ;;
   esac
 else
