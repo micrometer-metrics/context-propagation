359a82b197e9f5e4817aa5f7eaec1fec57c878fc
Dariusz Jędrzejczyk <dariusz.jedrzejczyk@broadcom.com>
2024-02-02 11:06:31 +0100

    Improved selective variant javadoc

--- context-propagation/src/main/java/io/micrometer/context/logging/slf4j/SelectiveMdcThreadLocalAccessor.java
+++ context-propagation/src/main/java/io/micrometer/context/logging/slf4j/SelectiveMdcThreadLocalAccessor.java
@@ -24,9 +24,9 @@ import org.slf4j.MDC;
 
 /**
  * {@link ThreadLocalAccessor} for copying values for selected keys from the {@link MDC}
- * across {@link Thread} boundaries. <strong>For use only when no other
- * {@link ThreadLocalAccessor} interacts with the MDC</strong>, for instance tracing
- * libraries.
+ * across {@link Thread} boundaries. Ensure no other
+ * {@link ThreadLocalAccessor} interacts with any MDC key that is used, for
+ * instance by tracing libraries.
  *
  * @author Dariusz Jędrzejczyk
  * @since 1.1.1
